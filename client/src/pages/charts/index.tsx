88ff0f977b23690c201a16b503f5959cbe341171
Junyi Cheng <rexcheng@umich.edu>
2022-05-03 10:21:57 -0700

    added URL support for charts page

--- client/src/pages/charts/index.tsx
+++ client/src/pages/charts/index.tsx
@@ -1,26 +1,51 @@
 import './style.scss';
 import 'components/ChartCard/style.scss';
 
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import ReactDom from 'react-dom';
 import _ from 'lodash';
+import qs from 'qs';
 import Navbar from 'components/Navbar';
 import { ChartDataObject } from 'components/ChartCard';
 import ChartPage from './ChartPage';
 import ChartListing from './ChartListing';
 
 const ChartsPage: FC = () => {
+  const queryParams = qs.parse(window.location.search, {
+    ignoreQueryPrefix: true,
+  });
+  const [renderListing, setRenderListing] = useState<boolean>(
+      !_.has(queryParams, 'chartId'),
+  );
   const [selectedChart, setSelectedChart] = useState<ChartDataObject>();
+
+  const updateSelectedChart = (chart: ChartDataObject) => {
+    setSelectedChart(chart);
+    setRenderListing(false);
+  };
 
   const clearSelectedChart = () => {
     setSelectedChart(undefined);
+    setRenderListing(true);
+    window.history.replaceState({}, document.title, window.location.pathname);
   };
+
+  useEffect(() => {
+    if (_.has(queryParams, 'chartId')) {
+      const chartId = _.get(queryParams, 'chartId');
+      fetch(`dump/chart-${chartId}.json`).then(
+          (response) => response.json(),
+      ).then((chart: ChartDataObject) => {
+        setSelectedChart(chart);
+      });
+    }
+  }, []);
 
   return (<>
     <Navbar base='.'/>
     {
-      _.isNil(selectedChart) ? (
-        <ChartListing onSelect={setSelectedChart}/>
+      renderListing || _.isNil(selectedChart) ? (
+        <ChartListing onSelect={updateSelectedChart}/>
       ) : (
         <ChartPage data={selectedChart} onGoBack={clearSelectedChart}/>
       )
